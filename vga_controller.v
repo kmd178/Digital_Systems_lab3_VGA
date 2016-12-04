ba9db0c0719f7bb9190a5d1dc2d54cf685d280a3
kmd178 <kmd178@gmail.com>
2016-12-04 05:13:03 +0200

    Modules connected and waveforms checked.  Ready for FPGA testing
    Comments and code clean up necessary

--- vga_controller.v
+++ vga_controller.v
@@ -2,9 +2,9 @@
 module vga_controller(
 	input reset, 
 	input clk, 
-	output reg VGA_RED,
-	output reg VGA_GREEN,
-	output reg VGA_BLUE,
+	output VGA_RED,
+	output VGA_GREEN,
+	output VGA_BLUE,
 	output reg VGA_HSYNC, 
 	output reg VGA_VSYNC);
 
@@ -14,8 +14,19 @@ module vga_controller(
 //    .CLK_IN1(clk),      // IN
 //    .CLK_OUT1(pixel_signal));    // OUT
 
-reg [9:0] count_pixels=0;
-reg [9:0] count_lines=0;
+reg [9:0] count_pixels=0;   //first 96+16 pixel time calculation give an empty result
+reg [9:0] count_lines=0;	 //first 10+2 line calculations give and empty result
+//wire  currentpixel= count_lines*800 + count_pixels;
+reg [20:0] virtual_out=0;
+reg [18:0] scaled_image_out=0;
+wire [18:0] pixeldata=scaled_image_out;
+reg [2:0] scaling_counter=0;
+
+vga_testImage_BRAM_initialization_RED bram1(clk,pixeldata,VGA_RED);
+vga_testImage_BRAM_initialization_GREEN bram2(clk,pixeldata,VGA_BLUE);
+vga_testImage_BRAM_initialization_BLUE bram3(clk,pixeldata,VGA_GREEN);
+
+
 
 reg [1:0] count_clocks=0; //every 2 clocks
 always @(posedge clk, posedge reset)
@@ -24,6 +35,10 @@ always @(posedge clk, posedge reset)
 	else 
 		count_clocks<=count_clocks+1'b1;	
 wire pixel_signal=count_clocks[0];
+
+
+
+
 //////////////////////
 always @(posedge pixel_signal, posedge reset)
 	if (reset) 
@@ -40,8 +55,7 @@ always @(posedge pixel_signal, posedge reset)
 		count_lines<=0;
 	else if (count_pixels==799)  ///NEW LINE PROCCESSING THE LAST PIXEL
 		count_lines<=count_lines+1'b1;
-		
-reg Change_pixel_flag=1; 
+
 //state machine. 
 //Count pixel 0 - 95  State0  signal HSYNC=0
 //Count pixel 96 - 143 State1 signal HSYNC=1
@@ -50,47 +64,84 @@ reg Change_pixel_flag=1;
 
 //VGA_HSYNC Signal timings and memory writing
 always @(posedge pixel_signal, posedge reset) 
-	if (reset) 
+	if (reset) 						  //RESET
 		VGA_HSYNC<=1;
 	else if (count_pixels<=95)   //PULSE WIDTH
 		VGA_HSYNC<=0;     
-	else if (count_pixels>=144 & count_pixels<=783) //DISPLAY TIME //96+48=144
-		begin
-			VGA_HSYNC<=1;
-			Change_pixel_flag<=1;
-		end
-	else 
-		begin						//BACK PORCH&FRONT PORCH
-			VGA_HSYNC<=1;
-			Change_pixel_flag<=0;
-		end
-		 //Cannot alter pixels
-
-//VGA_VSYNC Signal timings and memory writing		
+	else if (count_pixels>=144 & count_pixels<=783) //DISPLAY TIME //96+48=144//
+		VGA_HSYNC<=1;									
+	else 						//BACK PORCH&FRONT PORCH
+		VGA_HSYNC<=1;     
+	
+	//VGA_VSYNC Signal timings and memory writing		
 always @(posedge pixel_signal, posedge reset)
 	if (reset) 
-			VGA_VSYNC<=1;
-	else if (count_lines==1 | count_lines==0)
-			VGA_VSYNC<=0;	
-	else if (count_lines>=31 & count_lines<=510) 
 		begin
 			VGA_VSYNC<=1;
-			if (Change_pixel_flag)
+			virtual_out<=0;
+		end
+	else if (count_lines==1 | count_lines==0)			//PULSE WIDTH
+			VGA_VSYNC<=0;	
+	else if (count_lines>=31 & count_lines<=510) 
+		begin														//DISPLAY TIME
+			VGA_VSYNC<=1;
+			if (count_pixels>=144 & count_pixels<=783)
 				begin 
-					VGA_RED<=1;
-					VGA_BLUE<=0;
-					VGA_GREEN<=0;
-					//memory[count_pixels-16]<=count_pixels+1'b1;
+					virtual_out<=(count_pixels-144)+(count_lines-31)*640;  ///VERILOG DOESNT SUPPORT DIVISION FUNCTION. 
+					//The scalling problem could be really easily solved just by dividing with 5 and not taking account the result as the input vertical and horizontal coordinates
+				end
+			else
+				begin  //OUT OF SYNC SIGNAL: STATE INSIDE BACK PORCH&FRONT PORCH OF HSYNC SIGNAL
+					virtual_out<=0;
 				end
 		end
-	else 
-		begin   //OUT OF SYNC SIGNAL
+	else 			
+		begin   //OUT OF SYNC SIGNAL: STATE INSIDE BACK PORCH&FRONT PORCH OF VSYNC SIGNAL
 			VGA_VSYNC<=1;
-			VGA_RED<=1;
-			VGA_BLUE<=0;
-			VGA_GREEN<=0;
 		end
 			//Cannot alter pixels
+			
+
+reg [2:0] scaling_vertical_counter=0;		
+			
+always @(posedge pixel_signal, posedge reset)
+		if (reset) 
+			begin
+				scaled_image_out=0;
+				scaling_counter=0;
+				scaling_vertical_counter=0;
+			end
+		else if (count_lines>=31 & count_lines<=510)
+			begin
+				if (count_pixels>=145 & count_pixels<=784)
+					begin
+						if (scaling_counter==4)
+							begin
+								scaled_image_out=scaled_image_out+1;
+								scaling_counter=0;
+							end
+						else 
+							scaling_counter=scaling_counter+1;
+					end
+				else if (count_pixels==799)
+					begin 
+							if (scaling_vertical_counter!=4)
+								begin
+									scaling_vertical_counter=scaling_vertical_counter+1;
+									scaled_image_out=scaled_image_out-128;
+								end
+							else 
+								scaling_vertical_counter=0;
+					end
+				else
+					scaling_counter=0;
+			end
+		else
+			begin
+				scaling_vertical_counter=0;
+				scaled_image_out=0;
+			end
+
 
 
 //Pixel signal every 2 clocks //
